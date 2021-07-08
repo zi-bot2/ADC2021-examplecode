c9b5eb8ef5220be07335082474cffa27c9442806
emapuljak <puljak-ema96@hotmail.com>
2021-07-08 13:02:07 +0200

    working script

--- create_datasets.py
+++ create_datasets.py
@@ -1,14 +1,15 @@
 import argparse
 import h5py
 from sklearn.model_selection import train_test_split
+import numpy as np
 
-def create_datasets_dense(bkg_file, signals_file, signal_data_names, events, test_size=0.2, val_size=0.2, input_shape=57):
+def create_datasets_dense(bkg_file, output_bkg_name, signals_files, output_signal_names, events=None, test_size=0.2, val_size=0.2, input_shape=57):
     
     # read BACKGROUND data
     with h5py.File(bkg_file, 'r') as file:
-        full_data = h5f['Particles'][:,:,:-1]
-        np.random.shuffle(data)
-        data = data[:events,:,:]
+        full_data = file['Particles'][:,:,:-1]
+        np.random.shuffle(full_data)
+        if events: full_data = full_data[:events,:,:]
     
     # define training, test and validation datasets
     X_train, X_test = train_test_split(full_data, test_size=test_size, shuffle=True)
@@ -21,30 +22,28 @@ def create_datasets_dense(bkg_file, signals_file, signal_data_names, events, tes
     X_test = X_test.reshape(X_test.shape[0], input_shape)
     X_val = X_val.reshape(X_val.shape[0], input_shape)
     
-    with h5py.File(bkg_file + '_dataset.h5', 'w') as h5f:
+    with h5py.File(output_bkg_name + '_dataset.h5', 'w') as h5f:
         h5f.create_dataset('X_train', data = X_train)
         h5f.create_dataset('X_test', data = X_test)
         h5f.create_dataset('X_val', data = X_val)
     
-    if signal_file:
+    if signals_files:
         # read SIGNAL data
-        file2 = h5py.File(signals_file,'r')
-        for signal_data_name in signal_data_names:
-            sig_data_type = file2['%s' %(signal_data_name)][:]
-            sig_data_type = sig_data_type.reshape(sig_data_type.shape[0],input_shape)
-            with h5py.File(signal_data_name + '_dataset.h5', 'w') as h5f2:
-                h5f2.create_dataset('%s'% (signal_data_name), data = sig_data_type)    
-        file2.close()
-        
+        for i, signal_file in enumerate(signals_files):
+            f = h5py.File(signal_file,'r')
+            signal_data = f['Particles'][:,:,:-1]
+            signal_data = signal_data.reshape(signal_data.shape[0],input_shape)
+            with h5py.File(output_signal_names[i] + '_dataset.h5', 'w') as h5f2:
+                h5f2.create_dataset('Data', data = signal_data)        
     return
 
-def create_datasets_convolutional(bkg_file, signals_file, signal_data_names, events, test_size=0.2, val_size=0.2):
+def create_datasets_convolutional(bkg_file, output_bkg_name, signals_files, output_signal_names, events=None, test_size=0.2, val_size=0.2, input_shape=57):
     
     # read BACKGROUND data
     with h5py.File(bkg_file, 'r') as file:
-        full_data = h5f['Particles'][:,:,:-1]
-        np.random.shuffle(data)
-        data = data[:events,:,:]
+        full_data = file['Particles'][:,:,:-1]
+        np.random.shuffle(full_data)
+        if events: full_data = full_data[:events,:,:]
     
     # define training, test and validation datasets
     X_train, X_test = train_test_split(full_data, test_size=test_size, shuffle=True)
@@ -52,30 +51,30 @@ def create_datasets_convolutional(bkg_file, signals_file, signal_data_names, eve
 
     del full_data
     
-    with h5py.File(bkg_file + '_dataset.h5', 'w') as h5f:
+    with h5py.File(output_bkg_name + '_dataset.h5', 'w') as h5f:
         h5f.create_dataset('X_train', data = X_train)
         h5f.create_dataset('X_test', data = X_test)
         h5f.create_dataset('X_val', data = X_val)
     
-    if signal_file:
+    if signals_files:
         # read SIGNAL data
-        file2 = h5py.File(signals_file,'r')
-        for signal_data_name in signal_data_names:
-            sig_data_type = file2['%s' %(signal_data_name)][:]
-            sig_data_type = sig_data_type.reshape(sig_data_type.shape[0],input_shape)
-            with h5py.File(signal_data_name + '_dataset.h5', 'w') as h5f2:
-                h5f2.create_dataset('%s'% (signal_data_name), data = sig_data_type)    
-        file2.close()
+        for i, signal_file in enumerate(signals_files):
+            f = h5py.File(signal_file,'r')
+            signal_data = f['Particles'][:,:,:-1]
+            with h5py.File(output_signal_names[i] + '_dataset.h5', 'w') as h5f2:
+                h5f2.create_dataset('Data', data = signal_data)
         
     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bkg-file', type=str)
-    parser.add_argument('--signals-file', type=str)
-    parser.add_argument('--signal-names', type=str, action='append')
-    parser.add_argument('--test-size', type=float, default=0.2)
-    parser.add_argument('--val-size', type=float, default=0.2)
-    parser.add_argument('--input-shape', type=int, default=57)
+    parser.add_argument('--bkg_file', type=str)
+    parser.add_argument('--output_bkg_name', type=str)
+    parser.add_argument('--signals_files', type=str, action='append')
+    parser.add_argument('--output_signal_names', type=str, action='append')
+    parser.add_argument('--events', type=int, default=None)
+    parser.add_argument('--test_size', type=float, default=0.2)
+    parser.add_argument('--val_size', type=float, default=0.2)
+    parser.add_argument('--input_shape', type=int, default=57)
     args = parser.parse_args()
     create_datasets_dense(**vars(args))
